ed37d26b52c492ee2c28c8fa32fd69ccbc8c0951
Dave Brennan <davethebabe713@gmail.com>
2026-02-15 11:23:37 -0700

    Simple output changes

--- CSVtoJSON/csvtojson.py
+++ CSVtoJSON/csvtojson.py
@@ -5,10 +5,17 @@ import json
 inputFile = 'input.csv'
 
 # what is the file delimiter ex: comma ',' or tab '\t' or pipe '|'
-delimiter = '|'
+delimiter2 = '|'
+
+print("Opening up " + inputFile)
 
 with open(inputFile, mode='r', newline='', encoding='utf-8') as csvfile:
-    data = list(csv.DictReader(csvfile, delimiter = delimiter))
+    data = list(csv.DictReader(csvfile, delimiter = delimiter2))
+
+print("Opened and Saved Data")
 
 with open('output.json', mode='w', encoding='utf-8') as jsonfile:
     json.dump(data, jsonfile, indent=4)
+    
+print("json file opened and dumped")
+    
